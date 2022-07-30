de787783dfd4f12cd85ca06d6c6d23f597645527
AN <99376802+alainatividad@users.noreply.github.com>
2022-07-30 23:57:15 +1000

    Adding profile page for creating appointments

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import { useQuery } from "@apollo/client";
 import { GET_ME_CLIENT, GET_ME_CONSULTANT } from "../utils/queries";
 import { useUserContext } from "../utils/UserContext";
+import ConsultantSelect from "../components/ConsultantSelect";
 
 const profile = () => {
   const style = {
@@ -24,11 +25,14 @@ const profile = () => {
     );
   }
 
-  const { loading, error, data } =
+  const { loading, error, data, refetch } =
     state.user === "client"
       ? useQuery(GET_ME_CLIENT)
       : useQuery(GET_ME_CONSULTANT);
 
+  useEffect(() => {
+    refetch();
+  }, [state.selectedConsultant]);
   if (loading) {
     return <h2>LOADING...</h2>;
   }
@@ -44,12 +48,25 @@ const profile = () => {
   if (data) {
     const userData =
       (state.user === "client" ? data.meClient : data.meConsultant) || [];
+
     return (
       <div>
         <Container text style={style.margin}>
           <h2>
             Hi {userData.firstName} {userData.lastName}!
           </h2>
+          {!userData.scheduleDate ? (
+            <ConsultantSelect />
+          ) : (
+            <>
+              <h3>
+                Your next schedule is on{" "}
+                {new Date(userData.scheduleDate).toLocaleString("en-GB", {
+                  timezone: "Australia/Sydney",
+                })}
+              </h3>
+            </>
+          )}
         </Container>
       </div>
     );
