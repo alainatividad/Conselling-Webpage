22211cdfaddc41dea383210affd2c3d1ee035752
AN <99376802+alainatividad@users.noreply.github.com>
2022-07-31 00:08:42 +1000

    Fixed Home and added content

---
+++ client/src/components/HomeServices.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { Header, Image, Card } from "semantic-ui-react";
+
+const HomeServices = () => {
+  const style = {
+    h1: {
+      margin: "2em",
+    },
+    h2: {
+      margin: "4em 0em 2em",
+    },
+    h3: {
+      marginTop: "2em",
+      padding: "2em 0em",
+    },
+    last: {
+      marginBottom: "300px",
+    },
+  };
+  return (
+    <>
+      <Header
+        as="h1"
+        content="Our Services"
+        textAlign="center"
+        style={style.h1}
+      />
+      <Card.Group centered itemsPerRow={3}>
+        <Card>
+          <Image
+            wrapped
+            ui={false}
+            fluid
+            src={window.location.origin + "/img/home-individual.jpeg"}
+          />
+          <Card.Content>
+            <Card.Header textAlign="center">Individual Therapy</Card.Header>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Image
+            wrapped
+            ui={false}
+            fluid
+            src={window.location.origin + "/img/home-group.jpeg"}
+          />
+          <Card.Content>
+            <Card.Header textAlign="center">Group Therapy</Card.Header>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Image
+            wrapped
+            ui={false}
+            fluid
+            src={window.location.origin + "/img/home-socskills.jpeg"}
+          />
+          <Card.Content>
+            <Card.Header textAlign="center">Social Skills Group</Card.Header>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Image
+            wrapped
+            ui={false}
+            fluid
+            src={window.location.origin + "/img/home-familytherapy.jpeg"}
+          />
+          <Card.Content>
+            <Card.Header textAlign="center">Family Therapy</Card.Header>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Image
+            wrapped
+            ui={false}
+            fluid
+            src={window.location.origin + "/img/home-couplestherapy.jpeg"}
+          />
+          <Card.Content>
+            <Card.Header textAlign="center">Couples Therapy</Card.Header>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Image
+            wrapped
+            ui={false}
+            fluid
+            src={window.location.origin + "/img/home-personaldev.jpeg"}
+          />
+          <Card.Content>
+            <Card.Header textAlign="center">
+              Personal Development Workshop
+            </Card.Header>
+          </Card.Content>
+        </Card>
+      </Card.Group>
+    </>
+  );
+};
+
+export default HomeServices;
