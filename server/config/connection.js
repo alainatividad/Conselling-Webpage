437af3d0eea144fe44331599a11695a8f1876d64
AN <99376802+alainatividad@users.noreply.github.com>
2022-07-30 23:46:30 +1000

    Revert connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// mongoose.connect(
-//   process.env.MONGODB_URI || "mongodb://localhost/counsellingApp",
-mongoose.connect("mongodb://localhost/counsellingApp", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/counsellingApp",
+  {
+    // mongoose.connect("mongodb://localhost/counsellingApp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
