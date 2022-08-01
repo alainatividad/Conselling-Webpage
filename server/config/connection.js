c58615a0d9c5eb977d909a4fb241df2842c3ab74
AN <99376802+alainatividad@users.noreply.github.com>
2022-08-01 22:07:19 +1000

    Removed Fomantic + modified env vars

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/counsellingApp",
+  process.env.REACT_APP_MONGODB_URI || "mongodb://localhost/counsellingApp",
   {
     // mongoose.connect("mongodb://localhost/counsellingApp", {
     useNewUrlParser: true,
